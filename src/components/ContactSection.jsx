18d1b5377b4a25d3916e43ca6d3270707676da1d
imsunnnyyy <shashankshekhar4398@gmail.com>
2025-08-05 19:42:16 +0530

    Location Edit

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -100,7 +100,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium">Location</h4>
                   <a className="text-muted-foreground hover:text-primary transition-colors">
-                    India (Remote Friendly)
+                    Bengaluru(India)
                   </a>
                 </div>
               </div>
